e4dc762de8f62191a67043c1184653d83f5ec676
Jords <jnash51@live.com>
2025-02-23 22:00:17 +1000

    Add new template.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-// `pages/_app.js`
-import '../styles/global.css';
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default App;
